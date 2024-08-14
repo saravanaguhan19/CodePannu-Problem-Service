86776e0d1747a4114e0ebc51342be5b1c0714135
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-14 09:26:48 +0530

    Feat: Add mongoose db config

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const errorHandler = require("./utils/errorHandler");
+const connectToDB = require("./config/db.config");
 
 const app = express();
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-app.use("/api",apiRouter);
+app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
   return res.json({
@@ -23,6 +23,9 @@ app.get("/ping", (req, res) => {
 //last middleware if any error comes
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started at PORT: ${PORT} `);
+
+  await connectToDB();
+  console.log("Successfully connected to DB");
 });
