8dcfa77ff2f98832f95491bdde9abed2f9dd2ac9
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-16 21:02:45 +0530

    Feat: Configure logging for file , console and mongodb

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,3 +1,4 @@
+const logger = require("../config/logger.config");
 const NotFound = require("../errors/notFound.error");
 const { Problem } = require("../models");
 
@@ -50,7 +51,10 @@ class ProblemRepository {
   async deleteProblem(id) {
     try {
       const problem = await Problem.findByIdAndDelete(id);
-      if (!problem) throw new NotFound("Problem", id);
+      if (!problem) {
+        logger.error(`Problem with id: ${id} not found in the db`);
+        throw new NotFound("Problem", id);
+      }
       return problem;
     } catch (error) {
       console.log(error);
